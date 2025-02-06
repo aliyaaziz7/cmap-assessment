6449a29c4b855139bf85075995184d457baa07dc
Aliya Aziz <aliya.aziz@wedo-digital.co.uk>
2025-02-06 18:15:29 +0000

    Initial models

---
+++ Timesheet.Models/Timesheet.cs
@@ -0,0 +1,11 @@
+﻿namespace Timesheet.Models;
+
+public class Timesheet
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+    public DateTime Date { get; set; }
+    public int TotalHours => TimesheetRows != null ? TimesheetRows.Sum(x => x.HoursWorked) : 0;
+
+    public virtual ICollection<TimesheetRow>? TimesheetRows { get; set; }
+}
---
+++ Timesheet.Models/TimesheetRow.cs
@@ -0,0 +1,12 @@
+﻿namespace Timesheet.Models;
+
+public class TimesheetRow
+{
+    public int Id { get; set; }
+    public int TimesheetId { get; set; }
+    public required string ProjectName { get; set; }
+    public required string Description { get; set; }
+    public int HoursWorked { get; set; }
+
+    public virtual required Timesheet Timesheet { get; set; }
+}
