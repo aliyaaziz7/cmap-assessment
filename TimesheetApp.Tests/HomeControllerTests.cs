f47740834d417d4384d4a53984431a51d2b11a5e
Aliya Aziz <aliya.aziz@wedo-digital.co.uk>
2025-02-06 17:33:10 +0000

    Basic project structure

---
+++ TimesheetApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TimesheetApp.Controllers;
+using Moq;
+
+namespace TimesheetApp.Tests;
+
+public class HomeControllerTests
+{
+    private ILogger<HomeController> _logger;
+    
+    [SetUp]
+    public void Setup()
+    {
+        var mock = new Mock<ILogger<HomeController>>();
+        _logger = mock.Object;
+    }
+
+    [Test]
+    public void Test1()
+    {
+        var controller = new HomeController(_logger);
+        var result = controller.Index() as ViewResult;
+        Assert.That(result?.ViewName, Is.EqualTo("Index"));
+    }
+}
